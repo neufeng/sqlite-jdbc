5d23c04c8f650f844c1bfdf7510b378db66781c2
Steven <stevencheung2088@gmail.com>
2016-07-11 20:45:06 +0800

    windows sqlitejdbc库

--- src/test/java/org/sqlite/DecryptedTest.java
+++ src/test/java/org/sqlite/DecryptedTest.java
@@ -1,5 +1,6 @@
 package org.sqlite;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
 import java.io.File;
@@ -17,21 +18,33 @@ public class DecryptedTest {
 		// TODO Auto-generated method stub
 
 		try {
-			String filePath = "test.db";
+			String filePath = "/Users/zhangjun/Downloads/xypx/ershouchesanji.db";
 			String password = "123456";
 			
 			Class.forName("org.sqlite.JDBC");
 			
-			String url = "jdbc:sqlite:" + filePath;
-			Properties props = new Properties();
-	        props.put( "key", password );
-	        Connection conn = DriverManager.getConnection( url, props );
-	        Statement st = conn.createStatement();
-	        ResultSet rs = st.executeQuery( "select * from t_question where type = 1 limit 1" );
-	        while (rs.next()) {
-	        	System.out.println(rs.getString("title"));
-	        	System.out.println(rs.getString("answer_array"));
-	        }
+			String outputPath = "/Users/zhangjun/Downloads/xypx/test.db";
+			File file = File.createTempFile("test", ".db");
+			
+			String url = "jdbc:sqlite:" + file.getAbsolutePath();
+			Connection conn = DriverManager.getConnection(url);
+			Statement st = conn.createStatement();
+			String sql = String.format("ATTACH DATABASE '%s' AS encrypted KEY '%s';", filePath, password);
+			st.execute(sql);
+			st.execute("SELECT sqlcipher_export('encrypted');");
+			st.execute("DETACH DATABASE encrypted;");
+			
+			file.renameTo(new File(outputPath));
+			
+//			Properties props = new Properties();
+//	        props.put( "key", password );
+//	        Connection conn = DriverManager.getConnection( url, props );
+//	        Statement st = conn.createStatement();
+//	        ResultSet rs = st.executeQuery( "select * from t_question where type = 1 limit 1" );
+//	        while (rs.next()) {
+//	        	System.out.println(rs.getString("title"));
+//	        	System.out.println(rs.getString("answer_array"));
+//	        }
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
